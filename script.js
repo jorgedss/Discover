d62a0ac222736e55c55ab44318118823ce137a30
José Jorge <jorgesouza621@gmail.com>
2024-03-07 08:27:43 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  // //pegar a tag img
+  // const img = document.querySelector('#profile img')
+  // //substituir a imagem
+  // if (html.classList.contains("light")) {
+  // // se tiver no light mode, adicionar imagem light
+  // img.setAttribute('src',"./assets...")
+
+  // } else {
+  // // se tiver sem light mode, manter imagem 
+  // img.setAttribute("src", "./assets...")
+  // }
+
+  // caso fosse alterar o avatar:
+  // pegar a tag img
+
+  //substituir a imagem
+}
